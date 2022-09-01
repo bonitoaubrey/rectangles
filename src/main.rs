06e9cef6c2e310e9b44ab163082e75c5f125e743
Bonito Aubrey <bonitoaubrey@gmail.com>
2022-09-01 08:56:32 +0300

    using struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rectangle = (30,50);
+    let rectangle = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
@@ -7,6 +15,6 @@ fn main() {
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
